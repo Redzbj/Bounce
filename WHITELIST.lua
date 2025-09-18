74ce2a83420f113c8b9283a236f3da253879f851
Redzbj <938338551@qq.com>
2025-09-18 17:26:59 +0800

    Update WHITELIST.lua

--- WHITELIST.lua
+++ WHITELIST.lua
@@ -3,7 +3,10 @@ return {
     ["97fe20ab-df0a-4b23-ac9d-530da7eae67f"] = "bugup",
     ["9864d5d8-4ff1-4ae2-8c28-9b8c29553bf4"] = "Shirokoawa",
     ["058b7912-b536-4b53-a6ec-816294e96c5f"] = "12311211",
+    ["694fd58a-262c-48a1-bbd6-f47d08cdff64"] = "cjc978123",
 }
+}
+
 
 
 
